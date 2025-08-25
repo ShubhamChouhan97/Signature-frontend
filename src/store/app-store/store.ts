35021faa1e720a75125053a7a222cfc7b7e77272
ShubhamChouhan97 <shubhamsc9799@gmail.com>
2025-08-25 13:12:15 +0530

    store reset to orignal

--- src/store/app-store/store.ts
+++ src/store/app-store/store.ts
@@ -1,74 +1,8 @@
-// import { create } from "zustand";
-// import { immer } from "zustand/middleware/immer";
-// import { MainClient } from "../../client/quiz-client";
-// import { MessageInstance } from "antd/es/message/interface";
-// import { rolesMap } from "../../libs/statusMap";
-
-// interface TestStoreState {
-// 	session: Session | null;
-// 	appLoading: boolean;
-// 	messageInstance: MessageInstance | null;
-// 	init: () => Promise<void>;
-// 	setAppLoading: (_state: boolean) => void;
-// 	getRole: (_role: number) => string;
-// 	setMessageInstance: (_message: MessageInstance) => void;
-// }
-
-// export const createAppStore = (client: MainClient) => {
-// 	const initialValues: TestStoreState = {
-// 		session: null,
-// 		appLoading: false,
-// 		messageInstance: null,
-// 		init: async () => {},
-// 		setAppLoading: () => {},
-// 		setMessageInstance: () => {},
-// 		getRole: (role: number) => rolesMap[role as keyof typeof rolesMap],
-// 	};
-
-// 	return create<TestStoreState>()(
-// 		immer((set, get) => ({
-// 			...initialValues,
-// 			async init() {
-// 				const session = await client.getSession();
-
-// 				set({
-// 					session: session,
-// 				});
-// 			},
-// 			setAppLoading(state) {
-// 				set((appState) => {
-// 					appState.appLoading = state;
-// 					return appState;
-// 				});
-// 			},
-// 			setMessageInstance(instance) {
-// 				if (get().messageInstance) {
-// 					return;
-// 				}
-// 				set((appStore) => {
-// 					appStore.messageInstance = instance;
-// 					return appStore;
-// 				});
-// 			},
-// 		}))
-// 	);
-// };
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
+import { MainClient } from "../../client/quiz-client";
 import { MessageInstance } from "antd/es/message/interface";
 import { rolesMap } from "../../libs/statusMap";
-
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
-
-interface Session {
-	
-	userId: string;
-	role: number;
-	email: string;
-	phoneNumber: string;
-	name: string;
-};
-
 
 interface TestStoreState {
 	session: Session | null;
@@ -80,7 +14,7 @@ interface TestStoreState {
 	setMessageInstance: (_message: MessageInstance) => void;
 }
 
-export const createAppStore = () => {
+export const createAppStore = (client: MainClient) => {
 	const initialValues: TestStoreState = {
 		session: null,
 		appLoading: false,
@@ -95,26 +29,11 @@ export const createAppStore = () => {
 		immer((set, get) => ({
 			...initialValues,
 			async init() {
-				try {
-					const res = await fetch(`${backendUrl}/session`, {
-						method: "GET",
-						credentials: "include", // send cookies with request
-						headers: {
-							"Content-Type": "application/json",
-						},
-					});
+				const session = await client.getSession();
 
-					if (!res.ok) {
-						throw new Error("Failed to fetch session");
-					}
-
-					const session: Session = await res.json();
-
-					set({ session });
-				} catch (err) {
-					console.error("Session fetch error:", err);
-					set({ session: null });
-				}
+				set({
+					session: session,
+				});
 			},
 			setAppLoading(state) {
 				set((appState) => {
